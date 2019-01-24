90245bc6c4075a341126b6d643a0e81710987f44
张治政 <zhang15036896990@163.com>
2019-01-24 20:25:38 +0800

    类作用域，单例和多例

--- SpringDemo1/src/cn/spring2/Customer.java
+++ SpringDemo1/src/cn/spring2/Customer.java
@@ -1,5 +1,8 @@
 package cn.spring2;
 
 public class Customer {
-
+	public Customer() {
+		// TODO Auto-generated constructor stub
+		System.out.println("Customer类被实例化");
+	}
 }
--- SpringDemo1/src/cn/spring2/SpringTest3.java
+++ SpringDemo1/src/cn/spring2/SpringTest3.java
@@ -14,7 +14,10 @@ public class SpringTest3 {
 	public void demo1(){
 		ApplicationContext applicationContext=new ClassPathXmlApplicationContext("applicationContext.xml");
 		Customer customer1=(Customer) applicationContext.getBean("customer");
+		//在Spring IoC容器中仅存在一个Bean实例，Bean以单例方式存在
 		//通过打印对象的地址结果，发现一样，说明实例化一次
+		//默认的是单例
+		//如果在spring的xml文件把作用域从singleton改为prototype，实例化不是一次，地址也不一样
 		System.out.println(customer1);
 		System.out.println("=======================");
 		Customer customer2=(Customer) applicationContext.getBean("customer");
