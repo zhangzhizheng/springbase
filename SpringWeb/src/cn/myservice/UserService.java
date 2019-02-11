c94d6be916a2f7dabb885997e5c735e67ef1f51d
张治政 <zhang15036896990@163.com>
2019-02-11 15:59:50 +0800

    spring和web整合

---
+++ SpringWeb/src/cn/myservice/UserService.java
@@ -0,0 +1,7 @@
+package cn.myservice;
+
+public class UserService {
+	public void sayHello(){
+		System.out.println("hello spring web!!!");
+	}
+}
---
+++ SpringWeb/src/cn/myservlet/UserServlet.java
@@ -0,0 +1,48 @@
+package cn.myservlet;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import cn.myservice.UserService;
+
+/**
+ * Servlet implementation class UserServlet
+ */
+@WebServlet("/UserServlet")
+public class UserServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public UserServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		ApplicationContext applicationContext=new ClassPathXmlApplicationContext("applicationContext.xml");
+		UserService userService=(UserService) applicationContext.getBean("userService");
+		userService.sayHello();
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
